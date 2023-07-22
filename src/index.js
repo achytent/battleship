861bfd697313739954192065cea746a634f7c9af
achytent <achytent@gmail.com>
2023-07-22 08:12:41 +0300

    wip commit to push the project to other device

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-const Player = require('./player');
-const Board = require('./gameboard');
+import Player from './player.js';
+import Board from './gameboard.js';
+import './style.css';
+
 let currentMove = 0; // 0 for player, 1 for computer
 let gameover = false;
 
 const playerBoard = new Board();
+const computerBoard = new Board();
 
 function createPlayer(name) {
   const player = new Player(name);
 }
 
+//render
+
+function createLayout() {
+  const main = document.querySelector('main');
+}
+
+function nameModal() {}
+
+// Computer placing ships on the board
+
+// Player placing ships on the board
+
 while (!gameover) {
   if (currentMove === 0) {
-    /* 
+    /*
       Collect data about current move
     */
     player.makeMove();
+
+    // Check for gameover, for sunk ships
   }
 }
